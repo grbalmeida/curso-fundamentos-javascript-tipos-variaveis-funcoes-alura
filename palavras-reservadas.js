58d901458eeeb7bbf964e3aa601256b9888b7d4f
grbalmeida <g.r.almeida@live.com>
2023-08-23 20:26:40 -0300

    Palavras reservadas

---
+++ palavras-reservadas.js
@@ -0,0 +1,9 @@
+let cpfUsuario = "12312312312";
+
+// let var = 0; // Unexpected token 'var'
+// let if = 0; // Unexpected token 'if'
+// let const = "Alura"; // Unexpected token 'const'
+
+let varInicial = 0;
+let ifFalse = 0;
+let constDeTexto = "Alura";
