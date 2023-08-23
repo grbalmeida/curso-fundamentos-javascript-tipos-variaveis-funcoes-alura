0f0f004161c3bb014b610f497a33530f74ed3148
grbalmeida <g.r.almeida@live.com>
2023-08-23 20:49:08 -0300

    Erros e stacktrace

---
+++ erros.js
@@ -0,0 +1,6 @@
+// const numero; // SyntaxError: Missing initializer in const declaration
+const numero = 1;
+
+// console.log(numero // SyntaxError: missing ) after argument list
+
+// console.log(minhaVar); // ReferenceError: minhaVar is not defined
