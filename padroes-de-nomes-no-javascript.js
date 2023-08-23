60bb89ca44f1f138e065c520e4d3f6974baaa4d8
grbalmeida <g.r.almeida@live.com>
2023-08-23 17:50:42 -0300

    Padrão de nomes no JavaScript

---
+++ padroes-de-nomes-no-javascript.js
@@ -0,0 +1,7 @@
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR);
+// O Javascript trata os quatro exemplos abaixo como variáveis diferentes
