04a10e5f698c2749fbb949c0bc39764e3f9d1df9
grbalmeida <g.r.almeida@live.com>
2023-08-23 17:34:35 -0300

    Trabalhando com strings

---
+++ trabalhando-com-strings.js
@@ -0,0 +1,11 @@
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+console.log(cidade === input); // false
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
+
+const senha = "minhaSenha123";
+console.log(senha.length); // 13 caracteres
