7b4de37e05aef9820c3764477439c7094c82688f
grbalmeida <g.r.almeida@live.com>
2023-08-23 17:54:35 -0300

    Tipos null e undefined

---
+++ tipos-null-e-undefined.js
@@ -0,0 +1,15 @@
+let input = null;
+
+if (input === null) {
+    console.log('não há informação');
+} else {
+    console.log(input);
+}
+
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
+console.log(null == undefined); // true
+console.log(null === undefined); // false
