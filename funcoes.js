d582f58048388488c518dac1eb011bfe335fc3a0
grbalmeida <g.r.almeida@live.com>
2023-08-24 12:59:24 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+let x = "";
+console.log(x);
+x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) Declara a função
+
+                    // string
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// 2) Executa a função (1 ou + vezes)
+
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma());
+imprimeTexto(soma());
