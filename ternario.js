a05ed86eacdc3418041e0bea69e3a404743bff95
grbalmeida <g.r.almeida@live.com>
2023-08-24 11:57:42 -0300

    Operador ternário

---
+++ ternario.js
@@ -0,0 +1,10 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+if (idadeCliente >= 18) {
+    console.log("cerveja");
+} else {
+    console.log("suco");
+}
+                                 // true     // false
+console.log(idadeCliente >= 18 ? "cerveja" : "suco");
