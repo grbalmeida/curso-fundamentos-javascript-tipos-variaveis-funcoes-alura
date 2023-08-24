bb5b784394f7646909d917bec7a161d41692a470
grbalmeida <g.r.almeida@live.com>
2023-08-24 11:10:34 -0300

    Operadores de comparação

---
+++ comparacoes.js
@@ -0,0 +1,18 @@
+// == (comparação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto); // true
+
+// === (comparação explícita)
+
+console.log(numero === texto); // false
+
+// == só compara o valor
+// === compara o valor e o tipo de dado
+
+// tipos
+
+console.log(typeof numero); // number
+console.log(typeof texto); // string
