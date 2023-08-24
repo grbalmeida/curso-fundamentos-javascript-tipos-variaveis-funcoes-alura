0d5ff240dd2b2224afc4a2263fe662e60acf4ccf
grbalmeida <g.r.almeida@live.com>
2023-08-24 12:37:31 -0300

    Template literal

---
+++ template-strings.js
@@ -0,0 +1,8 @@
+const nome = "Ju";
+const idade = 2023 - 1981;
+const cidadeDeNascimento = "São Paulo";
+const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+const apresentacaoTemplateString = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
+console.log(apresentacaoTemplateString);
